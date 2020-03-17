7e6eec63bc01e423ec8cb39430aa9263ca72302b
pak-nikolai <pak-nikolai@mail.ru>
2020-03-17 23:20:25 +0500

    quality check 2

--- deploy-0.1.sh
+++ deploy-0.1.sh
@@ -6,6 +6,7 @@
 yum update -y
 yum upgrade -y
 yum install gcc -y
+yum install epel-release -y
 yum install python-pip -y
 yum install python3 -y
 pip install --upgrade pip
@@ -17,15 +18,15 @@ echo "userpassword2" | passwd --stdin nikolayp
 yum install -y nano
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install -y docker-ce
+yum install -y docker-ce docker-ce-cli containerd.io --no-best
 usermod -aG docker $(whoami)
 systemctl enable docker.service
 systemctl start docker.service
-yum install epel-release
-yum install -y python-pip.
 pip3 install -y docker-compose
 yum upgrade python*
 
+echo ''
+docker-compose -v
 echo ''
 echo "thats all"
 echo ''
