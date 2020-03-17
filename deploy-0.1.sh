647c1d1247cb1812ebd91845676a3e59ab13a33c
pak-nikolai <pak-nikolai@mail.ru>
2020-03-17 22:35:42 +0500

    change to pip3

--- deploy-0.1.sh
+++ deploy-0.1.sh
@@ -5,8 +5,10 @@
 #############################
 yum update -y
 yum upgrade -y
+yum install gcc -y
 yum install python-pip -y
 yum install python3 -y
+pip install --upgrade pip
 yum install git -y
 adduser user
 echo "userpassword2" | passwd --stdin user
@@ -15,11 +17,15 @@ echo "userpassword2" | passwd --stdin nikolayp
 yum install -y nano
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install docker-ce
+yum install -y docker-ce
 usermod -aG docker $(whoami)
 systemctl enable docker.service
 systemctl start docker.service
 yum install epel-release
 yum install -y python-pip.
-pip install docker-compose
+pip3 install -y docker-compose
 yum upgrade python*
+
+echo ''
+echo "thats all"
+echo ''
