06bfd9951575e56af8a1ac1fc482bb4c7af867cc
pak-nikolai <pak-nikolai@mail.ru>
2020-03-17 22:16:56 +0500

    initial commit

---
+++ deploy-0.1.sh
@@ -0,0 +1,25 @@
+#############################
+#   2020.03.17
+#   paknv   surgut sng
+#   version 0.1a
+#############################
+yum update -y
+yum upgrade -y
+yum install python-pip -y
+yum install python3 -y
+yum install git -y
+adduser user
+echo "userpassword2" | passwd --stdin user
+adduser nikolyp
+echo "userpassword2" | passwd --stdin nikolayp
+yum install -y nano
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install docker-ce
+usermod -aG docker $(whoami)
+systemctl enable docker.service
+systemctl start docker.service
+yum install epel-release
+yum install -y python-pip.
+pip install docker-compose
+yum upgrade python*
