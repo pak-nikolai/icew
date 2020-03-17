0a2c06974ca9acc917349e7c131e08ea673d98ae
pak-nikolai <pak-nikolai@mail.ru>
2020-03-17 23:21:45 +0500

    quality check 2.1

--- deploy-0.1.sh
+++ deploy-0.1.sh
@@ -18,7 +18,7 @@ echo "userpassword2" | passwd --stdin nikolayp
 yum install -y nano
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install -y docker-ce docker-ce-cli containerd.io --no-best
+yum install -y docker-ce docker-ce-cli containerd.io --nobest
 usermod -aG docker $(whoami)
 systemctl enable docker.service
 systemctl start docker.service
